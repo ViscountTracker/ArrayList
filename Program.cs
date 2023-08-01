d8e9c5832251f5c4360da9cb131ee4f0f011a701
Vladyslav Zaitsev <zaycev777vlad@gmail.com>
2023-08-01 09:07:33 +0300

    Implementation of methods such as Add, Get, Remove and Clear. Creating a task list.

--- Program.cs
+++ Program.cs
@@ -12,7 +12,10 @@ namespace ArrayList
         static void Main()
         {
             ArrayList<String> arList = new ArrayList<String>(2);
-
+            arList.Add("1");
+            arList.Add("2");
+            arList.Add("3");
+            arList.Clear();
         }
     }
     public class ArrayList<T>
@@ -34,5 +37,67 @@ namespace ArrayList
             PercentToDecrease = 0.25f;
         }
 
+        public void Add(T Value)
+        {
+            if (Count == Capacity)
+            {
+                Capacity = (int)(Capacity * MultiPlaer);
+                T[] TempArray = new T[Capacity];
+                for (int i = 0; i < Count; i++)
+                {
+                    TempArray[i] = Values[i];
+                }
+                Values = TempArray;
+                Console.WriteLine(Capacity);
+                Console.WriteLine(Count);
+                Console.WriteLine(Values.Length);
+            }
+            Values[Count] = Value;
+
+            Count++;
+        }
+
+        public T Get(int index)
+        {
+            if (index > Capacity)
+            {
+                throw new Exception("Not in range");
+            }
+            return Values[index];
+        }
+
+        public void Remove(T elementToRemove)
+        {
+            #region Step by step for create methods Remove()
+            // [1,2,3,4,5] -  inputArray,
+            // ElementToRemove = 3; - Index to remove + 1 = 2;
+            //tempArray - values copy in this array
+            //IndexToRemove= index with new values
+            //  inputArray.length - IndexToRemove -1;     
+            #endregion
+
+            int indexToRemove = Array.IndexOf(Values, elementToRemove);
+
+            if (indexToRemove >= 0)
+            {
+                var tempArray = new T[Capacity];
+                Array.Copy(Values, 0, tempArray, 0, indexToRemove);
+                Array.Copy(Values, indexToRemove + 1, tempArray, indexToRemove, Values.Length - indexToRemove - 1);
+                Count--;
+                Values = tempArray;
+            }
+            if (Count < PercentToDecrease * Capacity)
+            {
+                Capacity /= 2;
+                var tempArray = new T[Capacity];
+                Array.Copy(Values, 0, tempArray, 0, Count);
+                Values = tempArray;
+            }
+        }
+        public void Clear()
+        {
+            Count = 0;
+            Values = new T[Capacity];
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ArrayList
+# TODO List
+# //TODO: Sort() classic sort
+# //TODO: Method Insert(int index, item)
+# //TODO: Method AddRange(ArrayList<T>) add all elements in current array 
+# //TODO: Sort(bool SortDesc) if true descending order
